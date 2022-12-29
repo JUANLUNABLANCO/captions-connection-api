06dcdeb52e5eea3d3965a55acb1742c6eb323387
JUANLUNABLANCO <desarrolloaplicacionesweb.jmlb@gmail.com>
2022-12-29 22:40:54 +0100

    Captions connection proyect with node and mongodb, parte del servidor. captions-connection-api

---
+++ RAEDME.md
@@ -0,0 +1,55 @@
+# Captionsconnection
+
+  aplicacion realizada con node.js y mongodb
+
+  esta aplicacion hace parte de captions-connection-web realizada con angular
+  
+# captions-connection-api
+
+  https://github.com/JUANLUNABLANCO/captions-connection-api
+
+## CREATED BY
+
+Juan Manuel Luna Blanco
+[Instagram: gotth3way](https://www.instagram.com/gotth3way.apis/)
+[Instagram: Kryptonite](https://www.instagram.com/kryptonite.original/)
+
+[Linkedin](https://www.linkedin.com/in/juan-manuel-luna-blanco-180a1570/)
+
+[Youtube: desarrolladorhoy](https://www.youtube.com/channel/UCSEwIRkDJxLkbvKHOAcw_Xw)
+[Youtube: Kryptonite](https://www.youtube.com/channel/UCSEwIRkDJxLkbvKHOAcw_Xw)
+
+## INSTALACIONES
+
+	> node -v                 // v16.17.0
+	> npm -v                  // 8.15.0
+
+## Development server
+
+1. Run `npm run mongod` to serve mongod sevice. this service its running in port `30030`
+
+2. Run `npm run dev`to serve Nodejs server app. This api-rest-full its running in `http://localhost:3333/`
+
+
+## GIT CONFIGURATIONS
+
+	> git init
+	> git config --local user.email "desarrolloaplicacionesweb.jmlb@gmail.com"
+	> git config --local user.name "JUANLUNABLANCO"
+	> git branch -M  main
+
+	En este punto debes crear un repositorio nuevo en github vac铆o y enlazarlo
+
+	<!-- > git remote add origin https://github.com/JUANLUNABLANCO/<tu-repo>.git -->
+    
+    en caso que ya tuviésemos apuntando nuestro repo local a otro podemos borrar el origin así
+    > git remote remove origin
+
+	> git remote add origin https://github.com/JUANLUNABLANCO/captions-connection-api.git
+	> git config --list
+	 git add .
+	 git commit -m "Captions connection proyect with node and mongodb"
+	 git push -u origin main
+
+
+
---
+++ src/app_server/_connections/db_connect.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const db =
+    _ENV == "production" ?
+    require("../_configs/.db-env") :
+    require("../_configs/db");
+
+if (_ENV === "testing" || _ENV === "development") {
+    var uri = "mongodb://" + db.host + ":" + db.port + "/" + db.database;
+    console.log("URI-DB: " + uri);
+} else if (_ENV === "production") {
+    // #### MONGO_URL llega desde docker-compose.yml el otro es para productionHere sin docker
+    var uri =
+        process.env.MONGO_URL ||
+        "mongodb://" +
+        db.user +
+        ":" +
+        db.password +
+        "@" +
+        +db.host +
+        ":" +
+        db.port +
+        "/" +
+        db.database;
+    console.log("URI-DB: " + uri);
+}
+mongoose.set('strictQuery', false);
+
+// console.log("URI-DB: " + uri);
+mongoose.connect(uri, (error) => {
+    if (error) {
+        console.log('########## CONNECTION ERROR ##########', error)
+    } else {
+        console.log(
+            `connection to database, in ` +
+            _ENV +
+            `, established well: ` +
+            db.port
+        );
+        console.log(
+            "**********************************************************************"
+        );
+    }
+
+})
+
+
+// mongoose.connection
+//     .on("error", (err) => console.error.bind(console, "Connection:" + err)) // enlaza el track de error a la consola (proceso actual)
+//     .once("open", () => {
+//         console.log(
+//             `connection to database, in ` +
+//             _ENV +
+//             `, established well: ` +
+//             db.port
+//         );
+//         console.log(
+//             "**********************************************************************"
+//         );
+//     });
+
+module.exports = mongoose;
